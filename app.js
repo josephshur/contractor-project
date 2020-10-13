274086e27e3d690932eac4268b331235ca4cd10f
Joseph Shur <joehshur@gmail.com>
2020-10-13 13:55:32 -0700

    Stylized page with basic Bootstrap, and expanded on home page

--- app.js
+++ app.js
@@ -8,7 +8,20 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+//TEST ARRAY OF CHARITIES
+var charities = [
+  { title: "Placeholder Charity 1", desc: "Description Text", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "Placeholder Charity 2", desc: "Description Text", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "Placeholder Charity 3", desc: "Description Text", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
+
+//INDEX
+app.get('/charities', (req, res) => {
+  res.render('charities-index', { charities: charities });
 })
 
 const port = process.env.PORT || 3000;
